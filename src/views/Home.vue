6439ec1503fe3b84b830a9874f78f04665fcc81d
Helton Carlos <heltoncarlospa@hotmail.com>
2021-11-02 21:03:48 -0300

    cards dicas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="space-area">
     <CardBase />
-    <div class="flex">
-      <Slide />
-    </div>
+   
   </div>
 </template>
 
 <script>
 import CardBase from "@/components/CardBase.vue";
-import Slide from "@/components/Slide.vue";
+
 export default {
   name: "Home",
-  components: { CardBase, Slide },
+  components: { CardBase },
 };
 </script>
